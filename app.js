1ef0d5bb9a029eadee545c52fa25a45060f3d7c1
Krittiya Clark <krittiyaclark@gmail.com>
2018-06-20 16:39:03 -0400

    Add two images withn click counter

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const image = document.getElementById('image');
-const clickCount = document.getElementById('click-count');
+let image = document.getElementsByTagName('img');
+let clickCount = document.getElementById('click-count');
 let count = 0;
+let images = [...image];
+console.log(images);
 
-image.addEventListener('click', function(){
-  //the element has been clicked
-	count ++;
-	clickCount.innerHTML = `Click me: ${count}`
-}, false);
+// Loop through all images
+for (let i = 0; i < images.length; i++) {
+	let num = images[i];
+	// addEventListener to image
+	num.addEventListener('click', function(){
+			// Click count
+			count ++;
+        	clickCount.innerHTML = `CLICK ME: ${count}`;
+    });
+};
+	    
