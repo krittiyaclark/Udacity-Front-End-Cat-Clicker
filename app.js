83ea895f93151039fc1ed0b18607d0b253abf306
Krittiya Clark <krittiyaclark@gmail.com>
2018-06-21 17:18:06 -0400

    Set up data object

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-let image = document.getElementsByTagName('img');
 let clickCount = document.getElementById('click-count');
-let count = 0;
-let images = [...image];
-console.log(images);
+let data = {
+	cat-1: {
+				name: 'cat-1',
+				photo: 'image/cat-1.jpg',
+				count: 0
+			},
+	cat-2: {
+				name: 'cat-2',
+				photo: 'image/cat-2.jpg',
+				count: 0
+			}
+	};
+	
 
-// Loop through all images
-for (let i = 0; i < images.length; i++) {
-	let num = images[i];
+// console.log(imageArray);
+// let images = [...imageArray];
+let imageUI = '';
+
+
+const createImageItem = () => {
+	// Loop through all images
+	for (let i = 0; i < imageArray.length; i++) {
+		// console.log(imageArray);
+		// let createImage = document.getElementsByClassName('list');
+		let img = document.createElement("img");
+ 
+		img.src = imageArray;
+		let src = document.getElementsByClassName('list');
+		 
+		src.appendChild(img);
+		// createImage.appendChild(createLI);
+		// let num = image[i];
+	};
+};
+
+const imageEventListener = () => {
 	// addEventListener to image
-	num.addEventListener('click', function(){
-			// Click counter
-			count ++;
-        	clickCount.innerHTML = `CLICK ME: ${count}`;
+	createLI.addEventListener('click', function(){
+			clickCount.innerHTML = `CLICK ME: ${count}`;
     });
 };
+
+const clickCounter = () => {
+	// Click counter
+	count ++;
+
+}
+
+createImageItem();
 	    
