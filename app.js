41eb002221f7ce7e41d0f6d3e00963924031d56b
Krittiya Clark <krittiyaclark@gmail.com>
2018-06-27 18:33:21 -0400

    Update imageView

--- app.js
+++ app.js
@@ -16,7 +16,8 @@
 					photo: 'image/cat-2.jpg',
 					count: 0,
 					alt: 'cat'
-			  }
+			  	}
+			  ]
 		};
 
 	/* ======= Octopus ======= */
@@ -26,8 +27,10 @@
 		init: function() {
 			// set our current cat to the first one in the list
 			model.currentImage = model.cats[0];
+			// tell our views to initialize
+			imageView.init();
+			imageListView.init();
 		},
-		// tell our views to initialize
 
 		// getCurrentImage
 		getCurrentImage: function() {
@@ -44,6 +47,7 @@
 		// incrementCounter
 		incrementCounter: function() {
 			model.currentImage.count++;
+			imageView.render();
 
 		}
 	};
@@ -58,135 +62,81 @@
 				containerElem: '#cat',
 				nameElem: '#cat-name',
 				imageElem: '#cat-img',
-				countElem: '#cat-count'
-			},
+				countElem: '#cat-count',
+				imageListElem: '#cat-list'
+			};
 
 			this.catElem = document.getElementById(DOMstrings.containerElem);
 	        this.catNameElem = document.getElementById(DOMstrings.nameElem);
 	        this.catImageElem = document.getElementById(DOMstrings.imageElem);
 	        this.countElem = document.getElementById(DOMstrings.countElem);
-
+console.log(this.catImageElem)
 			// on click, increment the current cat's counter
 			this.catImageElem.addEventListener('click', function() {
 				octopus.incrementCounter();
 			});
-			
-			// render this view (update the DOM elements with the right values)
 
-		}
+			// render this view (update the DOM elements with the right values)
+			this.render();
+		},
 
 
 		render: function() {
-        // update the DOM elements with values from the current cat
-
+        	// update the DOM elements with values from the current cat
+        	let currentImage = octopus.getCurrentImage();
+        	this.DOMstrings.countElem.textContent = currentImage.count;
+        	this.catNameElem.textContent = currentImage.name;
+        	this.catImageElem.src = currentImage.photo;
     	}
 	};
 
-	let imageList = {
+	let imageListView = {
+		init: function() {
+		        // store the DOM element for easy access later
+		        this.catListElem = document.getElementById(imageView.init.DOMstrings.imageListElem);
 
-		// store the DOM element for easy access later
+		        // render this view (update the DOM elements with the right values)
+		        this.render();
+		    },
 
-		// render this view (update the DOM elements with the right values)
+		    render: function() {
+		        var cat, elem, i;
+		        // get the cats we'll be rendering from the octopus
+		        var cats = octopus.getImage();
 
-		// get the cats we'll be rendering from the octopus
+		        // empty the cat list
+		        this.catListElem.innerHTML = '';
 
-		// empty the cat list
+		        // loop over the cats
+		        for (i = 0; i < cats.length; i++) {
+		            // this is the cat we're currently looping over
+		            cat = cats[i];
 
-		 // loop over the cats
+		            // make a new cat list item and set its text
+		            elem = document.createElement('li');
+		            elem.textContent = cat.name;
 
-		 // this is the cat we're currently looping over
+		            // on click, setCurrentCat and render the catView
+		            // (this uses our closure-in-a-loop trick to connect the value
+		            //  of the cat variable to the click event function)
+		            elem.addEventListener('click', (function(catCopy) {
+		                return function() {
+		                    octopus.setCurrentImage(catCopy);
+		                    imageView.render();
+		                };
+		            })(cat));
 
-		 // make a new cat list item and set its text
+		            // finally, add the element to the list
+		            this.catListElem.appendChild(elem);
+		        }
+		    }
+    	}
 
-		 // on click, setCurrentCat and render the catView
-         // (this uses our closure-in-a-loop trick to connect the value
-         //  of the cat variable to the click event function)
-
-         // finally, add the element to the list
-	}
+// make it go!
+octopus.init();
 
 }());
 
-// make it go!
-
- 
-
-
-let clickCount = document.getElementById('click-count');
-let slideIndex = 1; 
-
-
-
-let imageUI = '';
-
-const createImageItem = (index) => {
-	// Loop through all images
-	let UL = document.getElementById('list');
-	let LI = document.createElement('li');
-	let IMG = document.createElement('img');
-	IMG.src = data.cats.photos[index];
-	LI.appendChild(document.createTextNode(data.cats.names[index]));
-	LI.appendChild(IMG);
-	UL.appendChild(LI);
-	// Add class to images
-	IMG.setAttribute('class', 'image');
-
-	let i;
-
-	// LI.setAttribute('class', 'mySlides');
-	// let slide = document.getElementsByClassName('list');
-	// if (index > slide.length) {
-	// 	slideIndex = 1;
-	// }
-	// 	if (index < 1) {
-	// 		slideIndex = slide.length;
-	// 	}
-	// 		for (i = 0; i < slide.length; i++) {
-	// 			slide[i].style.display = 'none';
-	// 		}
-	// 		slide[slideIndex - 1].style.display = 'block';
-};
-
-createImageItem(slideIndex);
-
-
-const imageEventListener = () => {
-	const button = document.getElementsByClassName('button');
-	let allImg = document.getElementsByTagName('img');
-	// addEventListener to image
-	button.addEventListener('click', function(){
-		createImageItem(0);
-		createImageItem(1);
-		clickCount.innerHTML = `CLICK ME: ${clickCounter}`;
-    });
-};
-
-const clickCounter = () => {
-	// Click counter
-	count ++;
-}  
-
-const currentImage = (n) => {
-	createImageItem(slideIndex = n);
-}
-
-// const showImages = (n) => {
-// 	createImageItem();
-// 	let i;
-
-// 	LI.setAttribute('class', 'mySlides');
-// 	let slide = document.getElementsByClassName('list');
-// 	if (n > slide.length) {
-// 		slideIndex = 1;
-// 	}
-// 		if (n < 1) {
-// 			slideIndex = slide.length;
-// 		}
-// 			for (i = 0; i < slide.length; i++) {
-// 				slide[i].style.display = 'none';
-// 			}
-// 			slide[slideIndex - 1].style.display = 'block';
-// }
 
 
 
